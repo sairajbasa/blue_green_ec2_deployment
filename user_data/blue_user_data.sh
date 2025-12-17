9901019e5dfbfc6e171dbee5ebf9e78c9ac7d4ce
sairajbasa <97380646+sairajbasa@users.noreply.github.com>
2025-12-17 22:26:34 +0530

    Create blue_user_data.sh

---
+++ user_data/blue_user_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Blue Environment - Limited Features
+FEATURE_LEVEL="basic"
+
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+
+HTML="/var/www/html/index.html"
+
+echo "<h1>Welcome to Our Application</h1>" > $HTML
+echo "<p>Blue Environment - Basic Version</p>" >> $HTML
+
+if [ "$FEATURE_LEVEL" = "advanced" ]; then
+  echo "<hr><h2>Dashboard</h2>" >> $HTML
+  echo "<button>Download Report</button>" >> $HTML
+fi
